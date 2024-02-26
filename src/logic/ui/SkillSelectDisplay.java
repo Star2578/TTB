9c6ab21b90c18f30fb8fb7d551bcfd8f8e21b3e6
Star2578 <nuttykingger@gmail.com>
2024-02-26 15:52:37 +0700

    improve skill selector UI

--- src/logic/ui/SkillSelectDisplay.java
+++ src/logic/ui/SkillSelectDisplay.java
@@ -4,11 +4,18 @@ import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Node;
 import javafx.scene.control.Label;
+import javafx.scene.control.Tooltip;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
+import javafx.scene.paint.Paint;
+import javafx.scene.shape.Circle;
+import javafx.scene.text.Text;
+import javafx.scene.text.TextAlignment;
 import logic.GameManager;
 import logic.ImageScaler;
 import logic.SceneManager;
@@ -28,6 +35,11 @@ public class SkillSelectDisplay implements Display{
     private GridPane skillSelectorGrid;
     private ImageScaler imageScaler = new ImageScaler();
 
+    private Text skillName;
+    private Text skillManaCost;
+    private Text skillActionPointCost;
+    private Text skillDescription;
+
     public SkillSelectDisplay() {
         view = new VBox();
         view.setPadding(new Insets(5));
@@ -38,9 +50,67 @@ public class SkillSelectDisplay implements Display{
         skillInfoBox = new VBox();
         skillInfoBox.setAlignment(Pos.CENTER);
         skillInfoBox.setSpacing(10);
-        Label titleLabel = new Label("Skill Name / Description / Costs");
-        titleLabel.setTextFill(Color.WHITE);
-        skillInfoBox.getChildren().add(titleLabel);
+
+        // TODO: Implement CSS so the fonts are works
+
+        // Initialize titles
+        Label nameTitle = new Label("Name: ");
+        nameTitle.setStyle(
+                "-fx-font-family:x16y32pxGridGazer;" +
+                "-fx-font-size:18;" +
+                "-fx-text-fill:'white';");
+        Label descriptionTitle = new Label("Description: ");
+        descriptionTitle.setStyle(
+                "-fx-font-family:x16y32pxGridGazer;" +
+                "-fx-font-size:18;" +
+                "-fx-text-fill:'white';");
+        Label manaTitle = new Label("Mana Cost: ");
+        manaTitle.setStyle(
+                "-fx-font-family:x16y32pxGridGazer;" +
+                "-fx-font-size:18;" +
+                "-fx-text-fill:'white';");
+        Label actionPointTitle = new Label("Action Point Cost: ");
+        actionPointTitle.setStyle(
+                "-fx-font-family:x16y32pxGridGazer;" +
+                "-fx-font-size:18;" +
+                "-fx-text-fill:'white';");
+
+        // Initialize skill info elements
+        skillName = new Text();
+        skillName.setWrappingWidth(280);
+        skillName.setTextAlignment(TextAlignment.CENTER);
+        skillName.setStyle(
+                "-fx-font-family:x16y32pxGridGazer;" +
+                "-fx-font-size:16;" +
+                "-fx-fill:'white';");
+        skillManaCost = new Text();
+        skillManaCost.setWrappingWidth(280);
+        skillManaCost.setTextAlignment(TextAlignment.CENTER);
+        skillManaCost.setStyle(
+                "-fx-font-family:x16y32pxGridGazer;" +
+                        "-fx-font-size:16;" +
+                        "-fx-fill:'white';");
+        skillActionPointCost = new Text();
+        skillActionPointCost.setWrappingWidth(280);
+        skillActionPointCost.setTextAlignment(TextAlignment.CENTER);
+        skillActionPointCost.setStyle(
+                "-fx-font-family:x16y32pxGridGazer;" +
+                        "-fx-font-size:16;" +
+                        "-fx-fill:'white';");
+        skillDescription = new Text();
+        skillDescription.setWrappingWidth(280);
+        skillDescription.setTextAlignment(TextAlignment.CENTER);
+        skillDescription.setStyle(
+                "-fx-font-family:x16y32pxGridGazer;" +
+                        "-fx-font-size:16;" +
+                        "-fx-fill:'white';");
+
+
+        // Add skill info elements to the skill info box
+        skillInfoBox.getChildren().addAll(
+                nameTitle, skillName, manaTitle, skillManaCost, actionPointTitle, skillActionPointCost, descriptionTitle, skillDescription
+        );
+        skillInfoBox.setPadding(new Insets(0, 0, 50, 0));
 
         // Initialize skill selector box
         skillSelectorGrid = new GridPane();
@@ -63,8 +133,13 @@ public class SkillSelectDisplay implements Display{
             }
         }
 
+        skillInfoBox.setAlignment(Pos.TOP_CENTER);
+        skillSelectorGrid.setAlignment(Pos.BOTTOM_CENTER);
         // Add skill info box and skill selector box to main view
         view.getChildren().addAll(skillInfoBox, skillSelectorGrid);
+        view.setMaxWidth(300);
+        view.setMinHeight(710);
+        view.setMaxHeight(720);
     }
 
     // This method create the skill frame from skill in parameter
@@ -94,11 +169,34 @@ public class SkillSelectDisplay implements Display{
                 SkillHandler.showValidSkillRange(player.getRow(), player.getCol(), skill);
                 GameManager.getInstance().updateCursor(SceneManager.getInstance().getGameScene(), Config.AttackCursor);
                 GameManager.getInstance().selectedSkill = skill;
+                updateSelectedSkillInfo();
                 System.out.println("Selected " + skill.getName() + " skill");
             });
         }
 
         return skillFrame;
+    }
+
+    // Method to update the selected skill information
+    private void updateSelectedSkillInfo(BaseSkill skill) {
+        if (skill != null) {
+            skillName.setText(skill.getName());
+            skillManaCost.setText(String.valueOf(skill.getManaCost()));
+            skillActionPointCost.setText(String.valueOf(skill.getActionPointCost()));
+            skillDescription.setText(skill.getDescription());
+        } else {
+            // Clear the skill info if no skill is selected
+            skillName.setText("");
+            skillManaCost.setText("");
+            skillActionPointCost.setText("");
+            skillDescription.setText("");
+        }
+    }
+
+    // Update method to update the selected skill information
+    public void updateSelectedSkillInfo() {
+        BaseSkill selectedSkill = GameManager.getInstance().selectedSkill;
+        updateSelectedSkillInfo(selectedSkill);
     }
 
     // Method to update skill frame into other skill
